651c3386072349fa903368f278088ab8f72d8d2b
RickyAnchores <ricky23anchores@gmail.com>
2022-01-24 22:55:21 +0000

    Added HashLink
    Added HashLinks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router,Switch } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 
 
@@ -9,12 +10,16 @@ import Home from './sections/Home/Home';
 
 const App = () => {
   return (
+    <Router>
     <div className='App'>
-      <Nav />
-      <Home />
-      <Contact />
-      <About />
+     
+        <Nav />
+     
+        <Home />
+        <Contact />
+        <About />
     </div>
+    </Router>
   )
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { HashLink as Link} from "react-router-hash-link"
 
 const Nav = () => {
+
+    const navList = [
+        {
+            name: "Home",
+            href: "#"
+        },
+        {
+            name: "Contact",
+            href: "#contact"
+        },
+        {
+            name: "About",
+            href: "#about"
+        }
+    ]
+
     return (
         <div className='Nav'>
             <h3>Nav</h3>
+            <ul className="navlinks">
+                {navList.map((link) => {
+                    return(
+                        <li>
+                            <Link to={link.href}>{link.name}</Link>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
